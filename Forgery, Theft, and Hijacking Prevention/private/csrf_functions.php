271dba2d93094de30441b9dd994993a5b73ba33f
Chantelle Levy <clevy000@gmail.com>
2017-02-27 16:33:06 -0500

    created and defined csrf tokens to be used throughout web application

--- Forgery, Theft, and Hijacking Prevention/private/csrf_functions.php
+++ Forgery, Theft, and Hijacking Prevention/private/csrf_functions.php
@@ -9,7 +9,8 @@
   // Returns HTML for a hidden form input with a CSRF token as the value
   function csrf_token_tag() {
     // TODO needs to set a token and put in an HTML tag
-    return '<input type="TODO" name="TODO" value="TODO" />';
+    $token = create_csrf_token();
+    return '<input type="hidden" name="csrf_token" value="' . $token . '" />';
   }
 
   // Returns true if form token matches session token, false if not.
@@ -26,4 +27,10 @@
     return true;
   }
 
+  function create_csrf_token() {
+    $token = csrf_token();
+    $_SESSION['csrf_token'] = $token;
+    $_SESSION['csrf_token_time'] = time();
+    return $token;
+  }
 ?>
