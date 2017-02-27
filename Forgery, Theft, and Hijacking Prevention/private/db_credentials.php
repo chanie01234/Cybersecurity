eb1a43ca01bf09d5a3ace25aea26cedeb6fde50f
Chantelle Levy <clevy000@gmail.com>
2017-02-27 15:26:53 -0500

    updated database creds

--- Forgery, Theft, and Hijacking Prevention/private/db_credentials.php
+++ Forgery, Theft, and Hijacking Prevention/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
   define("DB_SERVER", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "");
-  define("DB_NAME", "globitek");
+  define("DB_USER", "");
+  define("DB_PASS", "codepath");
+  define("DB_NAME", "globitek_wk4");
 ?>
