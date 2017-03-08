503ef6578d90c6bd93ba4b4f00794fab3e3b7e95
Chantelle Levy <clevy000@gmail.com>
2017-03-08 00:55:18 -0500

    repaired the dropbox to send messages to other agents and allowed automatic encryption of messages

--- Encryption/apex/public/agents/messages.php
+++ Encryption/apex/public/agents/messages.php
@@ -1,15 +1,11 @@
 <?php
-
   require_once('../../private/initialize.php');
-
   if(!isset($_GET['id'])) {
     redirect_to('index.php');
   }
-
   $id = $_GET['id'];
   $agent_result = find_agent_by_id($id);
   $agent = db_fetch_assoc($agent_result);
-
   $message_result = find_messages_for($agent['id']);
 ?>
 
@@ -23,16 +19,16 @@
     <link rel="stylesheet" media="all" href="<?php echo DOC_ROOT . '/includes/styles.css'; ?>" />
   </head>
   <body>
-    
+
     <a href="<?php echo url_for('/agents/index.php') ?>">Back to List</a>
     <br/>
 
     <h1>Messages for <?php echo h($agent['codename']); ?></h1>
-    
+
     <?php if($current_user['id'] == $agent['id']) { ?>
       <p>Your messages are automatically decrypted using your private key.</p>
     <?php } ?>
-    
+
     <table>
       <tr>
         <th>Date</th>
@@ -41,15 +37,20 @@
         <th>Message</th>
         <th>Signature</th>
       </tr>
-      
+
       <?php while($message = db_fetch_assoc($message_result)) { ?>
         <?php
+
           $created_at = strtotime($message['created_at']);
-          
-          // Oooops.
-          // My finger accidentally hit the delete-key.
-          // Sorry, APEX!!!
-          
+          $sender_result = find_agent_by_id($message['sender_id']);
+          $sender = db_fetch_assoc($sender_result);
+          if($current_user['id'] == $agent['id']) {
+            $message_text = pkey_decrypt($message['cipher_text'], $agent['private_key']);
+          } else {
+            $message_text = $message['cipher_text'];
+          }
+          $signature_result = verify_signature($message['cipher_text'], $message['signature'], $sender['public_key']);
+          $validity_text = $signature_result === 1 ? 'Valid' : 'Not valid';
         ?>
         <tr>
           <td><?php echo h(strftime('%b %d, %Y at %H:%M', $created_at)); ?></td>
@@ -60,6 +61,6 @@
         </tr>
       <?php } ?>
     </table>
-    
+
   </body>
 </html>
